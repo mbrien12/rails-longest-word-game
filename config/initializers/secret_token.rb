34e2ce89d11984be2395dfa4dd2b1466bce67ddb
Michelle Brien <michelle.brien12@gmail.com>
2017-11-13 13:26:17 -0200

    rails-new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsLongestWordGame::Application.config.secret_token = 'aa92bff03d8c846093850f2915e63535ed1683639b5ef53d6c2a0a71ef554fcfa629c534abce5f32015f9775cbfd8326ceaa61e588493d405e93a951d0bedf82'
